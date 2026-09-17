a21ac009c49533dbcd1aedbeec9b27d18f683e2b
Pratik Mahalle <mahallepratik683@gmail.com>
2026-09-17 10:00:16 +0530

    Update Cloudwake to 0.4.0

--- Casks/cloudwake.rb
+++ Casks/cloudwake.rb
@@ -1,6 +1,6 @@
 cask "cloudwake" do
-  version "0.3.3"
-  sha256 "61630177a5e27a31fd4b7e1f974c9020acdeefcd218d34663c40b169beabd1a3"
+  version "0.4.0"
+  sha256 "a1669ffe7b82989ab8f27b6b7c38f0d7e818de1f0e1aa71b345ae077692e5dfd"
 
   url "https://github.com/pratik-mahalle/infralive/releases/download/v#{version}/Cloudwake-#{version}-macos-arm64.zip"
   name "Cloudwake"
